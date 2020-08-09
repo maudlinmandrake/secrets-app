b481b9596fd1c9d179b39595ad9d01a1b35d3f04
Jennifer Mikac <jennymikac@gmail.com>
2020-08-09 13:51:37 -0400

    Create user schema

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
+const userSchema = {
+    email: String,
+    password: String
+}
+
 app.get("/", function(req, res){
     res.render("home");
 });
