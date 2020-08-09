7fa8f31f5dff12c8c832c8fbee9db1b358cdde4a
Jennifer Mikac <jennymikac@gmail.com>
2020-08-09 13:56:22 -0400

    Create new user via register info; render secrets

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const userSchema = {
 }
 
 const User = new mongoose.model("User", userSchema);
+
 app.get("/", function(req, res){
     res.render("home");
 });
@@ -30,8 +31,21 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.post("/register", function(req, res){
 
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
 
+    newUser.save(function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+})
 
 
 
