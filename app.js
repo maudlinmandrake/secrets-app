1f2fa1c998528ec64a3d478da2090f1e2bd1cd97
Jennifer Mikac <jennymikac@gmail.com>
2020-08-10 08:49:23 -0400

    Require dotenv

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+
 
 const app = express();
 
